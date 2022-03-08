42688d95a674c072f9c31f6875fdd64f525d3232
Nathaniel Pells <nathaniel.96.np@gmail.com>
2022-03-08 12:14:04 +0000

    refactored my topics components

--- src/Components/AllArticles/Articles.jsx
+++ src/Components/AllArticles/Articles.jsx
@@ -1,24 +1,25 @@
 import { useEffect, useState,  } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { getArticles } from "../../api";
 
-export default function Football() {
+
+export default function Articles() {
 
   const [articleList, setarticleList] = useState([]);
+  const {topic} = useParams();
 
   useEffect(()=> {
-    getArticles().then((articles) => {
-     const footballArticles = articles.filter(article => article.topic==="football")
-     setarticleList(footballArticles)
+    getArticles(topic).then((articles) => {
+     setarticleList(articles)
+
       
     })
-  }, [])
+  }, [topic])
 
   return( 
     <article className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
     <div className="tc">
-      <h2 className="f4" >Football Articles</h2>
-      
+      <h2 className="f4" >Articles</h2>
       </div>
       <ul>
         {articleList.map((article) => {
--- src/Components/AllArticles/IndividualArticle.jsx
+++ src/Components/AllArticles/IndividualArticle.jsx
@@ -1,4 +1,4 @@
-import AllArticles from "./AllArticles";
+
 
 export default function IndividualArticle() {
     return <h2>individual article</h2>
