b8657a126306dd9aa04bf1c8519319b63d4c37bf
Nathaniel Pells <nathaniel.96.np@gmail.com>
2022-03-08 15:47:28 +0000

    can view individual articles

--- src/Components/AllArticles/IndividualArticle.jsx
+++ src/Components/AllArticles/IndividualArticle.jsx
@@ -1,5 +1,32 @@
-
+import {getArticleById} from "../../api"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 
 export default function IndividualArticle() {
-    return <h2>individual article</h2>
-  }
+  const [article, setArticle] = useState('');
+  const { article_id } = useParams();
+
+  useEffect(() => {
+    getArticleById(article_id).then(({ article }) => {
+      setArticle(article);
+    });
+  }, [article_id]);
+
+  return (
+    <article className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+      <div className="tc">
+      <h2 className="f4" >{article.title}</h2>
+      </div>
+      <hr className="mw3 bb bw1 b--black-10"></hr>
+      <p className="lh-copy measure center f6 black-70">{article.body}</p>
+      <hr className="mw3 bb bw1 b--black-10"></hr>
+      <ul className="ArticleList" key={article.article_id}>
+        <li>Author: {article.author} </li>
+        <li>Comments: {article.comment_count} </li>
+        <li>Posted: {article.created_at} </li>
+        <li>Topic: {article.topic}</li>
+        <li>Votes: {article.votes}</li>
+      </ul>
+    </article>
+  );
+}
